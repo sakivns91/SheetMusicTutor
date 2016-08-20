b4761dc464ca56662a31f9dc42a514bb58db9741
vikas yadav <yadavikas@iitj.ac.in>
2016-08-21 00:14:33 +0530

    Initial commit

---
+++ app/src/main/java/com/musictutor/pianotutor/sheetmusictutor/MainActivity.java
@@ -0,0 +1,13 @@
+package pianosheetmusictutor.smtutor.com.sheetmusictutor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
