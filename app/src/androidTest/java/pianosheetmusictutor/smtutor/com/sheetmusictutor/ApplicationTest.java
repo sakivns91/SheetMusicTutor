a9505a11e58b4c04702f21aff5d79298fc437843
vikas yadav <yadavikas@iitj.ac.in>
2016-08-21 00:52:37 +0530

    changed folder structure

--- app/src/androidTest/java/pianosheetmusictutor/smtutor/com/sheetmusictutor/ApplicationTest.java
+++ app/src/androidTest/java/pianosheetmusictutor/smtutor/com/sheetmusictutor/ApplicationTest.java
@@ -1,4 +1,4 @@
-package pianosheetmusictutor.smtutor.com.sheetmusictutor;
+package com.musictutor.pianotutor.sheetmusictutor;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/musictutor/pianotutor/sheetmusictutor/MainActivity.java
+++ app/src/main/java/com/musictutor/pianotutor/sheetmusictutor/MainActivity.java
@@ -1,4 +1,4 @@
-package pianosheetmusictutor.smtutor.com.sheetmusictutor;
+package com.musictutor.pianotutor.sheetmusictutor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
